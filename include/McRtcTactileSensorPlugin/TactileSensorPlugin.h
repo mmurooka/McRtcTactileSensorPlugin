86eaaf2434a83b5f6bd03b7502a993759a59d1e0
Masaki Murooka <m-murooka@aist.go.jp>
2023-06-25 21:28:20 +0900

    Subscribe sensor msg.

--- include/McRtcTactileSensorPlugin/TactileSensorPlugin.h
+++ include/McRtcTactileSensorPlugin/TactileSensorPlugin.h
@@ -2,29 +2,47 @@
 
 #include <mc_control/GlobalPlugin.h>
 
+#include <ros/callback_queue.h>
+#include <ros/ros.h>
+
+#include <mujoco_tactile_sensor_plugin/TactileSensorData.h>
+
 namespace mc_plugin
 {
 
 /** \brief Tactile sensor plugin. */
 struct TactileSensorPlugin : public mc_control::GlobalPlugin
 {
+public:
   /** \brief Destructor. */
-  ~TactileSensorPlugin() override{};
+  ~TactileSensorPlugin();
 
   /** \brief Returns the plugin configuration. */
   mc_control::GlobalPlugin::GlobalPluginConfiguration configuration() override;
 
   /** \brief Initialize the plugin. */
-  void init(mc_control::MCGlobalController & controller, const mc_rtc::Configuration & config) override{};
+  void init(mc_control::MCGlobalController & controller, const mc_rtc::Configuration & config) override;
 
   /** \brief Reset the plugin. */
-  void reset(mc_control::MCGlobalController & controller) override{};
+  void reset(mc_control::MCGlobalController & controller) override;
 
   /** \brief Run before mc_control::MCGlobalController::run() */
-  inline void before(mc_control::MCGlobalController &) override {}
+  void before(mc_control::MCGlobalController & controller) override;
 
   /** \brief Run after mc_control::MCGlobalController::run() */
-  void after(mc_control::MCGlobalController & controller) override{};
+  void after(mc_control::MCGlobalController & controller) override;
+
+protected:
+  /** \brief ROS callback of sensor topic. */
+  void sensorCallback(const mujoco_tactile_sensor_plugin::TactileSensorData::ConstPtr & sensorMsg);
+
+protected:
+  //! ROS variables
+  //! @{
+  std::unique_ptr<ros::NodeHandle> nh_;
+  ros::CallbackQueue callbackQueue_;
+  ros::Subscriber sensorSub_;
+  //! @}
 };
 
 } // namespace mc_plugin
--- src/TactileSensorPlugin.cpp
+++ src/TactileSensorPlugin.cpp
@@ -1,9 +1,15 @@
+#include <mc_control/GlobalPluginMacros.h>
+
 #include <McRtcTactileSensorPlugin/TactileSensorPlugin.h>
 
 namespace mc_plugin
 {
 
-/** \brief Returns the plugin configuration. */
+TactileSensorPlugin::~TactileSensorPlugin()
+{
+  // \todo Set wrench zero
+}
+
 mc_control::GlobalPlugin::GlobalPluginConfiguration TactileSensorPlugin::configuration()
 {
   mc_control::GlobalPlugin::GlobalPluginConfiguration out;
@@ -13,4 +19,32 @@ mc_control::GlobalPlugin::GlobalPluginConfiguration TactileSensorPlugin::configu
   return out;
 }
 
+void TactileSensorPlugin::init(mc_control::MCGlobalController & controller, const mc_rtc::Configuration & config)
+{
+  std::string sensorTopicName = config("sensorTopicName", std::string("tactile_sensor"));
+
+  // Setup ROS
+  nh_ = std::make_unique<ros::NodeHandle>();
+  // Use a dedicated queue so as not to call callbacks of other modules
+  nh_->setCallbackQueue(&callbackQueue_);
+  sensorSub_ = nh_->subscribe<mujoco_tactile_sensor_plugin::TactileSensorData>(
+      sensorTopicName, 1, &TactileSensorPlugin::sensorCallback, this);
+}
+
+void TactileSensorPlugin::reset(mc_control::MCGlobalController & // controller
+)
+{
+}
+
+void TactileSensorPlugin::before(mc_control::MCGlobalController & controller) {}
+
+void TactileSensorPlugin::after(mc_control::MCGlobalController & // controller
+)
+{
+}
+
+void TactileSensorPlugin::sensorCallback(const mujoco_tactile_sensor_plugin::TactileSensorData::ConstPtr & sensorMsg) {}
+
 } // namespace mc_plugin
+
+EXPORT_MC_RTC_PLUGIN("TactileSensor", mc_plugin::TactileSensorPlugin)
