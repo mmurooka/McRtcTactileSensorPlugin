d5e152a0a237828b06552799381891e8fda52a02
Masaki Murooka <m-murooka@aist.go.jp>
2023-06-25 22:31:42 +0900

    Set dummy wrench value to a sensor.

--- include/McRtcTactileSensorPlugin/TactileSensorPlugin.h
+++ include/McRtcTactileSensorPlugin/TactileSensorPlugin.h
@@ -14,34 +14,35 @@ namespace mc_plugin
 struct TactileSensorPlugin : public mc_control::GlobalPlugin
 {
 public:
-  /** \brief Destructor. */
-  ~TactileSensorPlugin();
-
   /** \brief Returns the plugin configuration. */
   mc_control::GlobalPlugin::GlobalPluginConfiguration configuration() override;
 
   /** \brief Initialize the plugin. */
-  void init(mc_control::MCGlobalController & controller, const mc_rtc::Configuration & config) override;
+  void init(mc_control::MCGlobalController & gc, const mc_rtc::Configuration & config) override;
 
   /** \brief Reset the plugin. */
-  void reset(mc_control::MCGlobalController & controller) override;
+  void reset(mc_control::MCGlobalController & gc) override;
 
   /** \brief Run before mc_control::MCGlobalController::run() */
-  void before(mc_control::MCGlobalController & controller) override;
+  void before(mc_control::MCGlobalController & gc) override;
 
   /** \brief Run after mc_control::MCGlobalController::run() */
-  void after(mc_control::MCGlobalController & controller) override;
+  inline void after(mc_control::MCGlobalController & gc) override{};
 
 protected:
   /** \brief ROS callback of sensor topic. */
   void sensorCallback(const mujoco_tactile_sensor_plugin::TactileSensorData::ConstPtr & sensorMsg);
 
 protected:
+  //! Force sensor name
+  std::string forceSensorName_;
+
   //! ROS variables
   //! @{
   std::unique_ptr<ros::NodeHandle> nh_;
   ros::CallbackQueue callbackQueue_;
   ros::Subscriber sensorSub_;
+  std::shared_ptr<mujoco_tactile_sensor_plugin::TactileSensorData> sensorMsg_ = nullptr;
   //! @}
 };
 
--- src/TactileSensorPlugin.cpp
+++ src/TactileSensorPlugin.cpp
@@ -5,11 +5,6 @@
 namespace mc_plugin
 {
 
-TactileSensorPlugin::~TactileSensorPlugin()
-{
-  // \todo Set wrench zero
-}
-
 mc_control::GlobalPlugin::GlobalPluginConfiguration TactileSensorPlugin::configuration()
 {
   mc_control::GlobalPlugin::GlobalPluginConfiguration out;
@@ -19,9 +14,29 @@ mc_control::GlobalPlugin::GlobalPluginConfiguration TactileSensorPlugin::configu
   return out;
 }
 
-void TactileSensorPlugin::init(mc_control::MCGlobalController & controller, const mc_rtc::Configuration & config)
+void TactileSensorPlugin::init(mc_control::MCGlobalController & gc, const mc_rtc::Configuration & config)
 {
-  std::string sensorTopicName = config("sensorTopicName", std::string("tactile_sensor"));
+  const auto & controller = gc.controller();
+  const auto & robot = controller.robot();
+
+  // Load configuration
+  if(!config.has("sensorTopicName"))
+  {
+    mc_rtc::log::error_and_throw(
+        "[mc_plugin::TactileSensorPlugin] The \"sensorTopicName\" key must be specified in the configuration.");
+  }
+  std::string sensorTopicName = static_cast<std::string>(config("sensorTopicName"));
+  if(!config.has("forceSensorName"))
+  {
+    mc_rtc::log::error_and_throw(
+        "[mc_plugin::TactileSensorPlugin] The \"forceSensorName\" key must be specified in the configuration.");
+  }
+  forceSensorName_ = static_cast<std::string>(config("forceSensorName"));
+  if(!robot.hasForceSensor(forceSensorName_))
+  {
+    mc_rtc::log::error_and_throw("[mc_plugin::TactileSensorPlugin] No force sensor named \"{}\" exists.",
+                                 forceSensorName_);
+  }
 
   // Setup ROS
   nh_ = std::make_unique<ros::NodeHandle>();
@@ -29,21 +44,37 @@ void TactileSensorPlugin::init(mc_control::MCGlobalController & controller, cons
   nh_->setCallbackQueue(&callbackQueue_);
   sensorSub_ = nh_->subscribe<mujoco_tactile_sensor_plugin::TactileSensorData>(
       sensorTopicName, 1, &TactileSensorPlugin::sensorCallback, this);
+
+  reset(gc);
+
+  mc_rtc::log::success("[mc_plugin] Initialized TactileSensorPlugin.");
 }
 
-void TactileSensorPlugin::reset(mc_control::MCGlobalController & // controller
+void TactileSensorPlugin::reset(mc_control::MCGlobalController & // gc
 )
 {
+  sensorMsg_ = nullptr;
 }
 
-void TactileSensorPlugin::before(mc_control::MCGlobalController & controller) {}
-
-void TactileSensorPlugin::after(mc_control::MCGlobalController & // controller
-)
+void TactileSensorPlugin::before(mc_control::MCGlobalController & gc)
 {
+  auto & controller = gc.controller();
+  auto & robot = controller.robot();
+
+  // Call ROS callback
+  callbackQueue_.callAvailable(ros::WallDuration());
+
+  // Set measured wrench
+  robot.data()
+      ->forceSensors.at(robot.data()->forceSensorsIndex.at(forceSensorName_))
+      .wrench(sva::ForceVecd(Eigen::Vector3d(10, 0, 0),
+                             Eigen::Vector3d(10, 0, 0))); // \todo Calculate wrench from sensorMsg_
 }
 
-void TactileSensorPlugin::sensorCallback(const mujoco_tactile_sensor_plugin::TactileSensorData::ConstPtr & sensorMsg) {}
+void TactileSensorPlugin::sensorCallback(const mujoco_tactile_sensor_plugin::TactileSensorData::ConstPtr & sensorMsg)
+{
+  sensorMsg_ = std::make_shared<mujoco_tactile_sensor_plugin::TactileSensorData>(*sensorMsg);
+}
 
 } // namespace mc_plugin
 
